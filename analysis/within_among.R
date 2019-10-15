65094ac3e045f8cb5eae4866319d3a65d1d8c53a
Sean Anderson <sean@seananderson.ca>
2019-10-15 14:38:41 -0700

    Within/across with spatial working?

--- analysis/within_among.R
+++ analysis/within_among.R
@@ -98,13 +98,16 @@ parameters_nonspatial$ln_cv  <- set_par_value(tmb_opt, "ln_cv")
 
 
 obj_nonspatial <- MakeADFun(data=data_nonspatial, parameters=parameters_nonspatial,
-  random=random_nonspatial,
-  hessian=FALSE, DLL=VersionNonSpatial)
+  random=random_nonspatial, DLL=VersionNonSpatial)
 
-tmb_opt <- nlminb(
+opt_nonspatial <- nlminb(
   start = obj_nonspatial$par, objective = obj_nonspatial$fn, gradient = obj_nonspatial$gr,
   control = list(eval.max = 1000, iter.max = 1000)
 )
+
+SD <- TMB::sdreport(obj_nonspatial)
+s <- summary(SD)
+
 
 # opt_nonspatial = TMBhelper::Optimize(obj=obj_nonspatial,
 #                                      control=list(eval.max=1000, iter.max=1000),
@@ -113,17 +116,17 @@ tmb_opt <- nlminb(
 # SD = sdreport( obj_nonspatial )
 # final_gradient = obj_nonspatial$gr( opt_nonspatial$par )
 # if( any(abs(final_gradient)>0.0001) | SD$pdHess==FALSE ) stop("Not converged")
-#
-# ParHat_nonspatial = as.list( opt_nonspatial$SD, "Estimate" )
-# SEHat_nonspatial  = as.list( opt_nonspatial$SD, "Std. Error" )
-#
-# cbind("slope"=(ParHat_nonspatial$eps_slope + ParHat_nonspatial$mu_slope),"se"=SEHat_nonspatial$eps_slope)
-#
-# ParHat_nonspatial$mu_slope
-# SEHat_nonspatial$mu_slope
-#
-# ParHat_nonspatial$b_j_omega #first value is among-lake coefficient
-# SEHat_nonspatial$b_j_omega
+
+ParHat_nonspatial = as.list( opt_nonspatial$SD, "Estimate" )
+SEHat_nonspatial  = as.list( opt_nonspatial$SD, "Std. Error" )
+
+cbind("slope"=(ParHat_nonspatial$eps_slope + ParHat_nonspatial$mu_slope),"se"=SEHat_nonspatial$eps_slope)
+
+ParHat_nonspatial$mu_slope
+SEHat_nonspatial$mu_slope
+
+ParHat_nonspatial$b_j_omega #first value is among-lake coefficient
+SEHat_nonspatial$b_j_omega
 
 
 #----------
@@ -185,6 +188,7 @@ tmb_map <- list(
   ln_sd_tzero =  as.factor(rep(NA, length(parameters_spatial$ln_sd_tzero))),
   ln_sd_slope =  as.factor(rep(NA, length(parameters_spatial$ln_sd_slope))),
   ln_tau_O =     as.factor(rep(NA, length(parameters_spatial$ln_tau_O))),
+  ln_kappa = as.factor(rep(NA, length(parameters_spatial$ln_kappa))),
   rho =          as.factor(rep(NA, length(parameters_spatial$rho)))
 )
 
@@ -199,25 +203,78 @@ tmb_opt <- nlminb(
   control = list(eval.max = 1000, iter.max = 1000)
 )
 
+set_par_value <- function(opt, par) {
+  as.numeric(opt$par[par == names(opt$par)])
+}
+parameters_spatial$ln_global_omega <- set_par_value(tmb_opt, "ln_global_omega")
+parameters_spatial$ln_global_linf <- set_par_value(tmb_opt, "ln_global_linf")
+parameters_spatial$global_tzero <- set_par_value(tmb_opt, "global_tzero")
+parameters_spatial$ln_b_sex   <- set_par_value(tmb_opt, "ln_b_sex")
+parameters_spatial$b_j_omega <- set_par_value(tmb_opt, "b_j_omega")
+parameters_spatial$mu_slope <- set_par_value(tmb_opt, "mu_slope")
+parameters_spatial$ln_cv  <- set_par_value(tmb_opt, "ln_cv")
+
+obj_spatial2 = MakeADFun(data=data_spatial, parameters=parameters_spatial,
+  random=random_spatial, DLL=VersionSpatial)
+
+
+tmb_opt2 <- nlminb(
+  start = obj_spatial2$par, objective = obj_spatial2$fn, gradient = obj_spatial2$gr,
+  control = list(eval.max = 1000, iter.max = 1000)
+)
+
+get_convergence_diagnostics <- function(sd_report) {
+  final_grads <- sd_report$gradient.fixed
+  bad_eig <- FALSE
+  if (!is.null(sd_report$pdHess)) {
+    if (!sd_report$pdHess) {
+      warning("The model may not have converged: ",
+        "non-positive-definite Hessian matrix.", call. = FALSE)
+    } else {
+      eigval <- try(1 / eigen(sd_report$cov.fixed)$values, silent = TRUE)
+      if (is(eigval, "try-error") || (min(eigval) < .Machine$double.eps * 10)) {
+        warning("The model may not have converged: ",
+          "extreme or very small eigen values detected.", call. = FALSE)
+        bad_eig <- TRUE
+      }
+      if (any(final_grads > 0.01))
+        warning("The model may not have converged. ",
+          "Maximum final gradient: ", max(final_grads), ".", call. = FALSE)
+    }
+  }
+  list(final_grads = final_grads, bad_eig = bad_eig)
+}
+
+
+
+# SD <- TMB::sdreport(obj_spatial2)
+# s <- summary(SD)
+
 # -------------------------
 
 #about ten minutes to run on my machine:
-opt_spatial  = TMBhelper::Optimize(obj=obj_spatial,
-                                   control=list(eval.max=1000, iter.max=1000),
-                                   getsd=T, newtonsteps=1, bias.correct=T)
-                                   # lower=c(rep(-Inf,12),-0.999), upper=c(rep(Inf,12),0.999))
+# opt_spatial  = TMBhelper::Optimize(obj=obj_spatial,
+#                                    control=list(eval.max=1000, iter.max=1000),
+#                                    getsd=T, newtonsteps=1, bias.correct=T)
+#                                    # lower=c(rep(-Inf,12),-0.999), upper=c(rep(Inf,12),0.999))
 
-SD = sdreport( obj_spatial )
-final_gradient = obj_spatial$gr( opt_spatial$par )
-if( any(abs(final_gradient)>0.0001) | SD$pdHess==FALSE ) stop("Not converged")
+SD = sdreport( obj_spatial2 )
 
-ParHat = as.list( opt_spatial$SD, "Estimate" )
-SEHat  = as.list( opt_spatial$SD, "Std. Error" )
+SD$gradient.fixed
+get_convergence_diagnostics(SD)
+
+# final_gradient = obj_spatial2$gr( obj_spatial2$par )
+# if( any(abs(final_gradient)>0.0001) | SD$pdHess==FALSE ) stop("Not converged")
+
+# ------------------------------
+
+ParHat = as.list( SD, "Estimate" )
+SEHat  = as.list( SD, "Std. Error" )
 ParHat$eps_slope
 SEHat$eps_slope
 
 cbind("slope"=(ParHat$eps_slope + ParHat$mu_slope),"se"=SEHat$eps_slope)
-opt_spatial
+tmb_opt2
 
 ParHat$mu_slope
 SEHat$mu_slope
