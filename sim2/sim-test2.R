e77f2ec905e7fc449da00ffcdbf23d9c57906684
ChrisFishCahill <christopher.cahill@ucalgary.ca>
2020-04-02 21:19:25 -0600

    bump pwalk

--- sim2/sim-test2.R
+++ sim2/sim-test2.R
@@ -276,7 +276,10 @@ fit_sim <- function(Nyears = 10, Nlakes = 15, Nfish = 20,
 #   sig_varies_fitted = c("ar1 st")
 # )
 # set.seed(1)
-# out = purrr::pmap_dfr(totest, fit_sim, silent = F) # testing
+# out <- purrr::pmap_dfr(totest, fit_sim, silent = F) # testing
+# out <- furrr::future_pmap_dfr(totest, fit_sim,
+# .options = future_options(seed = 123L) #for testing parallel
+
 
 # Visualize the priors (penalties) for ar1 st model:
 tau_O_mean_prior <- 0
@@ -342,57 +345,15 @@ run_sim_experiment <- function(rho = rho, kappa = kappa,
 }
 
 totest <- tidyr::expand_grid(
-  iter = seq_len(10L),
+  iter = seq_len(300L),
   sig_varies = c("by lake", "by time", "both", "ar1 st"),
   sig_varies_fitted = c("by lake", "by time", "both", "ar1 st")
 )
 
-system.time({
-  pwalk(pars_to_sim, run_sim_experiment)
-})
 
-# End end end
-#--------
-#--------
-
-# out <- furrr::future_pmap_dfr(totest, fit_sim,
-# .options = future_options(seed = 123L) #for testing
-
-# which failed?
-buggered <- out %>% dplyr::filter(convergence == 1)
-print(buggered, n = Inf)
-whichSims <- buggered$iter
-message(paste0(
-  paste0(length(whichSims), " out of "),
-  length(unique(totest$iter)), " iterations did not converge"
-))
-
-table(buggered$sig_varies)
-
-out %>%
-  dplyr::filter(!(iter %in% whichSims)) %>%
-  dplyr::mutate(sig_varies = paste0("Sim = ", sig_varies)) %>%
-  dplyr::mutate(sig_varies_fitted = paste0("Fitted = ", sig_varies_fitted)) %>%
-  ggplot(aes(ln_global_omega)) +
-  geom_histogram(bins = 30) +
-  geom_vline(xintercept = out[["true_ln_global_omega"]][1]) +
-  facet_grid(sig_varies_fitted ~ sig_varies) +
-  xlab(expression(omega)) +
-  ylab("Count")
-ggsave("sim2/hist-sim.pdf", width = 7, height = 5)
-
-out$sig_varies_fitted <- factor(out$sig_varies_fitted, levels = c("by lake", "by time", "both", "ar1 st"))
-out$sig_varies <- factor(out$sig_varies, levels = c("by lake", "by time", "both", "ar1 st"))
-
-out %>%
-  dplyr::filter(!(iter %in% whichSims)) %>%
-  dplyr::mutate(Matched = ifelse(sig_varies_fitted == sig_varies, TRUE, FALSE)) %>%
-  ggplot(aes(sig_varies, exp(ln_global_omega), colour = sig_varies_fitted, fill = Matched)) +
-  geom_boxplot() +
-  geom_hline(yintercept = exp(out[["true_ln_global_omega"]][1])) +
-  xlab(expression(Simulated ~ omega ~ variation)) +
-  labs(colour = expression(Fitted ~ omega ~ variation)) +
-  scale_color_brewer(palette = "Dark2") +
-  scale_fill_manual(values = c("white", "grey60")) +
-  ylab(expression(omega[0]))
-ggsave("sim2/boxplot-sim.pdf", width = 7, height = 5)
+if (FALSE) {
+  system.time({
+    # 4.73 hrs on Cahill's desktop
+    purrr::pwalk(pars_to_sim, run_sim_experiment)
+  })
+}
