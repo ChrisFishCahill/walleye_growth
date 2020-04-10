37658e99bbdeef0e333565e60d5002c50b591cc3
Christopher Cahill <you@example.com>
2020-04-10 15:01:55 -0600

    bump cpp and initial case study fits

--- analysis2/case-study.R
+++ analysis2/case-study.R
@@ -1,2 +1,96 @@
+library(ggplot2)
+theme_set(theme_light())
+library(TMB)
+library(future)
+library(tidyr)
+library(INLA)
+library(purrr)
+library(furrr)
+library(dplyr)
+library(TMBhelper)
 
 data <- readRDS("analysis2/vB_analysis_august_2019_cahill.rds")
+TMB::compile("analysis2/vb_alta.cpp")
+
+Loc <- unique(data[ ,c("X_TTM_c","Y_TTM_c") ] ) / 1000 #Put distance in kms
+
+#mesh = inla.mesh.2d(loc=loc_xy, max.edge=c(62,1000)) #Better mesh, but slower
+mesh = inla.mesh.create( Loc, refine=TRUE, extend=-0.5, cutoff=0.01 ) #faster mesh
+
+mesh$n
+
+#png(file="Mesh.png",width=9.50,height=7.00,units="in",res=600)
+plot(mesh)
+points(Loc, col="Steelblue", pch=1)
+
+spde <- INLA::inla.spde2.matern(mesh, alpha = 2)
+spdeMatrices <- spde$param.inla[c("M0", "M1", "M2")]
+
+rho_sd_prior = 2
+rho_mean_prior = 0
+tau_O_mean_prior = 0
+tau_O_sd_prior = 3
+
+data <- list(
+  Nobs = nrow(data),
+  length_i = data$TL,
+  age_i = data$Age,
+  lake_i = data$Lake - 1L,
+  time_i = data$Year - 1L,
+  Nlakes = length(unique(data$Lake)),
+  spdeMatrices = spdeMatrices,
+  rho_sd_prior = rho_sd_prior,
+  rho_mean_prior = rho_mean_prior,
+  tau_O_mean_prior = tau_O_mean_prior,
+  tau_O_sd_prior = tau_O_sd_prior
+)
+
+parameters <- list(
+  ln_global_linf = log(55),
+  ln_sd_linf = 0,
+  global_tzero = -1,
+  ln_sd_tzero = 0,
+  ln_global_omega = log(15),
+  ln_sd_omega_lake = 0,
+  ln_sd_omega_time = 0,
+  eps_omega_lake = rep(0, data$Nlakes),
+  eps_omega_time = rep(0, length(unique(data$time_i))),
+  eps_linf = rep(0, data$Nlakes),
+  eps_t0 = rep(0, data$Nlakes),
+  eps_omega_st = matrix(0, nrow = mesh$n, ncol = length(unique(data$time_i))),
+  ln_cv = log(0.069),
+  ln_kappa = log(3),
+  ln_tau_O = 0,
+  rho_unscaled = 3.4
+)
+
+map <- list(
+  eps_omega_time = as.factor(rep(NA, length(unique(data$time_i)))),
+  ln_sd_omega_time = factor(NA),
+    eps_omega_lake = as.factor(rep(NA, data$Nlakes)),
+    ln_sd_omega_lake = factor(NA)
+  )
+
+dyn.load(dynlib("analysis2/vb_alta"))
+
+obj <- TMB::MakeADFun(data, parameters,
+                      DLL = "vb_alta",
+                      random = c("eps_omega_lake", "eps_omega_time", "eps_omega_st", "eps_linf", "eps_t0"),
+                      map = map
+)
+
+opt = nlminb(obj$par, obj$fn, obj$gr,
+             eval.max = 1000, iter.max = 1000)
+sdrep = sdreport( obj )
+final_gradient = obj$gr( opt$par )
+if( any(abs(final_gradient)>0.01) || sdrep$pdHess==FALSE ) stop("Not converged")
+
+rangeIndex = which(row.names(summary(sdrep,"report"))=="range")
+range = summary(sdrep,"report")[rangeIndex,]
+
+
+
+rep <- sdreport(obj, bias.correct=T)
+
+
+
---
+++ analysis2/vb_alta.cpp
@@ -0,0 +1,118 @@
+#include <TMB.hpp>
+/*
+* Cahill & Anderson 20 April 2020
+*/
+
+template <class Type>
+bool isNA(Type x)
+{
+  return R_IsNA(asDouble(x));
+}
+
+template <class Type>
+Type dlnorm(Type x, Type meanlog, Type sdlog, int give_log = 0)
+{
+  Type logres;
+  logres = dnorm(log(x), meanlog, sdlog, true) - log(x);
+  if (give_log)
+    return logres;
+  else
+    return exp(logres);
+}
+
+template <class Type>
+Type objective_function<Type>::operator()()
+{
+  using namespace density;
+  using namespace R_inla;
+  using namespace Eigen;
+
+  // Data
+  DATA_INTEGER(Nobs);            // Observations in the dataset
+  DATA_VECTOR(length_i);         // length fish i
+  DATA_VECTOR(age_i);            // age of fish i
+  DATA_IVECTOR(lake_i);          // grouping factor for lake
+  DATA_IVECTOR(time_i);          // grouping factor for time
+  DATA_INTEGER(Nlakes);          // Number of lakes
+  DATA_SCALAR(rho_sd_prior);     // rho SD prior
+  DATA_SCALAR(rho_mean_prior);   // rho mean prior
+  DATA_SCALAR(tau_O_sd_prior);   // tau_O_ SD prior
+  DATA_SCALAR(tau_O_mean_prior); // tau_O_ mean prior
+
+  // SPDE objects
+  DATA_STRUCT(spdeMatrices, spde_t);
+
+  // Parameters
+  PARAMETER(ln_global_linf);
+  PARAMETER(ln_sd_linf);
+
+  PARAMETER(global_tzero);
+  PARAMETER(ln_sd_tzero);
+
+  PARAMETER(ln_global_omega);
+  PARAMETER(ln_sd_omega_lake);
+  PARAMETER(ln_sd_omega_time);
+
+  // Random coefficients
+  PARAMETER_VECTOR(eps_omega_lake);
+  PARAMETER_VECTOR(eps_omega_time);
+  PARAMETER_VECTOR(eps_linf);
+  PARAMETER_VECTOR(eps_t0);
+  PARAMETER_ARRAY(eps_omega_st);
+
+  // Likelihood noise term
+  PARAMETER(ln_cv);
+
+  // Spatial-temporal terms
+  PARAMETER(ln_kappa);     //matern kappa
+  PARAMETER(ln_tau_O);     //spatial noise
+  PARAMETER(rho_unscaled); //autocorrelation term
+
+  // back-transform rho
+  Type rho = Type(2)*invlogit(rho_unscaled)-Type(1);
+
+  vector<Type> length_pred(Nobs);
+  Type jnll = 0;
+
+  // Probability of random coefficients:
+  for (int l = 0; l < Nlakes; l++) {
+    jnll -= dnorm(eps_linf(l), Type(0.0), exp(ln_sd_linf), true);
+    jnll -= dnorm(eps_t0(l), Type(0.0), exp(ln_sd_tzero), true);
+    jnll -= dnorm(eps_omega_lake(l), Type(0.0), exp(ln_sd_omega_lake), true);
+  }
+  for (int t = 0; t < eps_omega_time.size(); t++) {
+    jnll -= dnorm(eps_omega_time(t), Type(0.0), exp(ln_sd_omega_time), true);
+  }
+  SparseMatrix<Type> Q = R_inla::Q_spde(spdeMatrices, exp(ln_kappa));
+  jnll += SCALE(SEPARABLE(AR1(rho), GMRF(Q)), 1.0 / exp(ln_tau_O))(eps_omega_st);
+
+  // penalties:
+  jnll -= dnorm(rho, rho_mean_prior, rho_sd_prior, true);
+  jnll -= dnorm(ln_tau_O, tau_O_mean_prior, tau_O_sd_prior, true);
+
+  // Derived quantities
+  Type Range = sqrt(8) / exp(ln_kappa);
+  Type SigO = 1 / sqrt(4 * M_PI * exp(2*ln_tau_O) * exp(2*ln_kappa));
+
+  for (int i = 0; i < Nobs; i++) {
+    Type omega = exp(ln_global_omega +     // intercept
+      eps_omega_lake(lake_i(i)) +          // std lake ran eff
+      eps_omega_time(time_i(i)) +          // std time ran eff
+      eps_omega_st(lake_i(i), time_i(i))); // ar1 space time
+
+    Type linf = exp(ln_global_linf +       // intercept
+      eps_linf(lake_i(i)));                // std ran eff
+
+    Type t0 = global_tzero +      // intercept
+      eps_t0(lake_i(i));         // std ran eff
+
+    length_pred(i) = linf * (1 - exp(-(omega / linf) * (age_i(i) - t0)));
+
+    if (!isNA(length_i(i)))
+      jnll -= dlnorm(length_i(i), log(length_pred(i)), exp(ln_cv), true);
+  }
+  return jnll;
+  ADREPORT(Range)
+  ADREPORT(SigO)
+}
+
