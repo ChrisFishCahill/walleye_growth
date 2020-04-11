7d92fa7b7e17811cdc09e9f8ab427d5f3f62c338
ChrisFishCahill <christopher.cahill@ucalgary.ca>
2020-04-11 14:01:44 -0600

    bump get_fit function

--- analysis2/case-study.R
+++ analysis2/case-study.R
@@ -12,85 +12,202 @@ library(TMBhelper)
 data <- readRDS("analysis2/vB_analysis_august_2019_cahill.rds")
 TMB::compile("analysis2/vb_alta.cpp")
 
-Loc <- unique(data[ ,c("X_TTM_c","Y_TTM_c") ] ) / 1000 #Put distance in kms
+Loc <- unique(data[, c("X_TTM_c", "Y_TTM_c")]) / 1000 # Put distance in kms
 
-#mesh = inla.mesh.2d(loc=loc_xy, max.edge=c(62,1000)) #Better mesh, but slower
-mesh = inla.mesh.create( Loc, refine=TRUE, extend=-0.5, cutoff=0.01 ) #faster mesh
+# mesh = inla.mesh.2d(loc=loc_xy, max.edge=c(62,1000)) #Better mesh, but slower
+mesh <- inla.mesh.create(Loc, refine = TRUE, extend = -0.5, cutoff = 0.01) # faster mesh
 
 mesh$n
 
-#png(file="Mesh.png",width=9.50,height=7.00,units="in",res=600)
+# png(file="Mesh.png",width=9.50,height=7.00,units="in",res=600)
 plot(mesh)
-points(Loc, col="Steelblue", pch=1)
+points(Loc, col = "Steelblue", pch = 1)
 
 spde <- INLA::inla.spde2.matern(mesh, alpha = 2)
 spdeMatrices <- spde$param.inla[c("M0", "M1", "M2")]
 
-rho_sd_prior = 2
-rho_mean_prior = 0
-tau_O_mean_prior = 0
-tau_O_sd_prior = 3
-
-data <- list(
-  Nobs = nrow(data),
-  length_i = data$TL,
-  age_i = data$Age,
-  lake_i = data$Lake - 1L,
-  time_i = data$Year - 1L,
-  Nlakes = length(unique(data$Lake)),
-  spdeMatrices = spdeMatrices,
-  rho_sd_prior = rho_sd_prior,
-  rho_mean_prior = rho_mean_prior,
-  tau_O_mean_prior = tau_O_mean_prior,
-  tau_O_sd_prior = tau_O_sd_prior
-)
-
-parameters <- list(
-  ln_global_linf = log(55),
-  ln_sd_linf = 0,
-  global_tzero = -1,
-  ln_sd_tzero = 0,
-  ln_global_omega = log(15),
-  ln_sd_omega_lake = 0,
-  ln_sd_omega_time = 0,
-  eps_omega_lake = rep(0, data$Nlakes),
-  eps_omega_time = rep(0, length(unique(data$time_i))),
-  eps_linf = rep(0, data$Nlakes),
-  eps_t0 = rep(0, data$Nlakes),
-  eps_omega_st = matrix(0, nrow = mesh$n, ncol = length(unique(data$time_i))),
-  ln_cv = log(0.069),
-  ln_kappa = log(3),
-  ln_tau_O = 0,
-  rho_unscaled = 3.4
-)
-
-map <- list(
-  eps_omega_time = as.factor(rep(NA, length(unique(data$time_i)))),
-  ln_sd_omega_time = factor(NA),
-    eps_omega_lake = as.factor(rep(NA, data$Nlakes)),
-    ln_sd_omega_lake = factor(NA)
+get_fit <- function(Linf = 55, T0 = -1, SigO = 1.0, cv = 0.05, omega_global = 15,
+                     rho = 0.5, kappa = 3.0,
+                     sig_varies_fitted = c("fixed", "by lake", "by time", "both", "ar1 st"),
+                     iter = NA, silent = TRUE,
+                     rho_sd_prior = 2, rho_mean_prior = 0,
+                     tau_O_mean_prior = 0, tau_O_sd_prior = 3) {
+  sig_varies_fitted <- match.arg(sig_varies_fitted)
+  cat(
+    crayon::green(
+      clisymbols::symbol$tick
+    ),
+    fitted = "model fitted = ", sig_varies_fitted,
+    sep = ""
   )
 
-dyn.load(dynlib("analysis2/vb_alta"))
+  Partition_i <- rep(0, nrow(data)) # use all the data
 
-obj <- TMB::MakeADFun(data, parameters,
-                      DLL = "vb_alta",
-                      random = c("eps_omega_lake", "eps_omega_time", "eps_omega_st", "eps_linf", "eps_t0"),
-                      map = map
-)
+  data <- list(
+    Nobs = nrow(data),
+    length_i = data$TL,
+    age_i = data$Age,
+    lake_i = data$Lake - 1L,
+    time_i = data$Year - 1L,
+    Nlakes = length(unique(data$Lake)),
+    sex_i = data$SexCode,
+    X_ij_omega = model.matrix(~ 1 + data$wallEffDen.Std + data$compEffDen.Std +
+      data$GDD.Std + data$wallEffDen.Std:data$compEffDen.Std),
+    spdeMatrices = spdeMatrices,
+    predTF_i = Partition_i,
+    rho_sd_prior = rho_sd_prior,
+    rho_mean_prior = rho_mean_prior,
+    tau_O_mean_prior = tau_O_mean_prior,
+    tau_O_sd_prior = tau_O_sd_prior
+  )
 
-opt = nlminb(obj$par, obj$fn, obj$gr,
-             eval.max = 1000, iter.max = 1000)
-sdrep = sdreport( obj )
-final_gradient = obj$gr( opt$par )
-if( any(abs(final_gradient)>0.01) || sdrep$pdHess==FALSE ) stop("Not converged")
+  parameters <- list(
+    ln_global_linf = log(Linf),
+    ln_sd_linf = -2.05361277,
+    global_tzero = T0,
+    ln_sd_tzero = 0,
+    ln_b_sex = 1,
+    b_j_omega = rep(1, ncol(data$X_ij_omega)),
+    ln_sd_omega_lake = 0,
+    ln_sd_omega_time = 0,
+    eps_omega_lake = rep(0, data$Nlakes),
+    eps_omega_time = rep(0, length(unique(data$time_i))),
+    eps_linf = rep(0, data$Nlakes),
+    eps_t0 = rep(0, data$Nlakes),
+    eps_omega_st = matrix(0, nrow = mesh$n, ncol = length(unique(data$time_i))),
+    ln_cv = log(cv),
+    ln_kappa = log(kappa),
+    ln_tau_O = log(SigO),
+    rho_unscaled = 3
+  )
+  map <- list()
+  if (sig_varies_fitted %in% c("fixed", "by lake", "ar1 st")) {
+    map <- c(map, list(
+      eps_omega_time = as.factor(rep(NA, length(unique(data$time_i)))),
+      ln_sd_omega_time = factor(NA)
+    ))
+  }
+  if (sig_varies_fitted %in% c("fixed", "by time", "ar1 st")) {
+    map <- c(map, list(
+      eps_omega_lake =  as.factor(rep(NA, data$Nlakes)),
+      ln_sd_omega_lake = factor(NA)
+    ))
+  }
+  if (sig_varies_fitted != "ar1 st") {
+    map <- c(map, list(
+      eps_omega_st = as.factor(matrix(NA, nrow = mesh$n, ncol = length(unique(data$time_i)))),
+      ln_kappa = factor(NA),
+      rho_unscaled = factor(NA),
+      ln_tau_O = factor(NA)
+    ))
+  }
+  if (!"vb_alta" %in% names(getLoadedDLLs())) {
+    cat(crayon::blue(clisymbols::symbol$star), "Loading DLL\n")
+    dyn.load(dynlib("analysis2/vb_alta"))
+  }
 
-rangeIndex = which(row.names(summary(sdrep,"report"))=="range")
-range = summary(sdrep,"report")[rangeIndex,]
+  obj <- TMB::MakeADFun(data, parameters,
+    DLL = "vb_alta",
+    random = c("eps_omega_lake", "eps_omega_time", "eps_omega_st", "eps_linf", "eps_t0"),
+    map = map,
+    silent=silent
+  )
 
+  opt <- nlminb(obj$par, obj$fn, obj$gr,
+    eval.max = 1000, iter.max = 1000
+  )
+  rep <- TMB::sdreport(obj)
+  final_gradient = obj$gr( opt$par )
+  #if( any(abs(final_gradient)>0.01) | rep$pdHess==FALSE ) {
+    #browser()
+    #opt$par[which(abs(final_gradient)>0.01)]
+    #rep$pdHess
+   # } #stop("Not converged")
+  rep
+}
 
+by_lake = get_fit(sig_varies_fitted = "by lake", silent=F)
+by_time = get_fit(sig_varies_fitted = "by time", silent=F)
+both = get_fit(sig_varies_fitted = "both", silent=F)
+ar1 = get_fit(sig_varies_fitted = "ar1 st", silent=F)
 
-rep <- sdreport(obj, bias.correct=T)
-
-
-
+# rep$pdHess
+#
+# final_gradient = obj$gr( opt$par )
+# if( any(abs(final_gradient)>0.01) | rep$pdHess==FALSE ) stop("Not converged")
+#
+#
+#
+# opt$par[which(abs(final_gradient)>0.01)]
+#
+#   map <- list(
+#     #ln_sd_tzero = factor(NA),
+#     ln_sd_linf = factor(NA),
+#     eps_linf = as.factor(rep(NA, data$Nlakes))
+#     #eps_t0 = as.factor(rep(NA, data$Nlakes))
+#   )
+#
+# map <- c(map, list(
+#   eps_omega_time = as.factor(rep(NA, length(unique(data$time_i)))),
+#   ln_sd_omega_time = factor(NA),
+#   eps_omega_lake = as.factor(rep(NA, data$Nlakes)),
+#   ln_sd_omega_lake = factor(NA)
+# ))
+# map <- c(map, map$rho_unscaled = factor(NA))
+# map <- map$rho_unscaled <- factor(NA)
+# map
+#
+# obj <- TMB::MakeADFun(data, parameters,
+#   DLL = "vb_alta",
+#   random = c("eps_omega_st", "eps_linf", "eps_t0"), # "eps_omega_lake", "eps_omega_time",
+#   map = map
+# )
+#
+# opt <- nlminb(obj$par, obj$fn, obj$gr,
+#   eval.max = 1000, iter.max = 1000
+# )
+#
+# rep <- TMB::sdreport(obj)
+# rep$pdHess
+#
+# final_gradient = obj$gr( opt$par )
+# if( any(abs(final_gradient)>0.01) | rep$pdHess==FALSE ) stop("Not converged")
+#
+#
+# parameters$rho_unscaled <- qlogis((0.99 + 1) / 2)
+#
+#
+#
+#
+#
+# opt
+#
+# str(rep)
+#
+# obj$report()
+#
+# r <- obj$report()
+#
+# str(opt)
+# str(obj)
+#
+#
+#
+# summary(rep, select = "report")
+# as.list(rep, what = "Std. Error")
+#
+#
+# str(rep)
+#
+# obj$report(obj$env$last.par.best)
+#
+#
+# opt <- TMBhelper::fit_tmb(
+#   obj = obj,
+#   control = list(eval.max = 1000, iter.max = 1000),
+#   getsd = T, newtonsteps = 1, bias.correct = T
+# )
+# SD <- sdreport(obj_spatial)
+#
+# AIC <- c(-2 * -opt$objective + 2 * length(opt$par))
+# # 191947.4
--- analysis2/vb_alta.cpp
+++ analysis2/vb_alta.cpp
@@ -1,6 +1,6 @@
 #include <TMB.hpp>
 /*
-* Cahill & Anderson 20 April 2020
+* Cahill & Anderson 11 April 2020
 */
 
 template <class Type>
@@ -28,19 +28,21 @@ Type objective_function<Type>::operator()()
   using namespace Eigen;
 
   // Data
-  DATA_INTEGER(Nobs);            // Observations in the dataset
-  DATA_VECTOR(length_i);         // length fish i
-  DATA_VECTOR(age_i);            // age of fish i
-  DATA_IVECTOR(lake_i);          // grouping factor for lake
-  DATA_IVECTOR(time_i);          // grouping factor for time
-  DATA_INTEGER(Nlakes);          // Number of lakes
-  DATA_SCALAR(rho_sd_prior);     // rho SD prior
-  DATA_SCALAR(rho_mean_prior);   // rho mean prior
-  DATA_SCALAR(tau_O_sd_prior);   // tau_O_ SD prior
-  DATA_SCALAR(tau_O_mean_prior); // tau_O_ mean prior
+  DATA_INTEGER(Nobs);                  // Observations in the dataset
+  DATA_VECTOR(length_i);               // length fish i
+  DATA_VECTOR(age_i);                  // age of fish i
+  DATA_IVECTOR(lake_i);                // grouping factor for lake
+  DATA_IVECTOR(time_i);                // grouping factor for time
+  DATA_INTEGER(Nlakes);                // Number of lakes
+  DATA_IVECTOR(sex_i);                 // factor for sex
+  DATA_MATRIX(X_ij_omega);             // covariate matrix for omega
+  DATA_STRUCT(spdeMatrices, spde_t);   // SPDE objects
+  DATA_VECTOR(predTF_i);               // Indicator for CV fold
 
-  // SPDE objects
-  DATA_STRUCT(spdeMatrices, spde_t);
+  DATA_SCALAR(rho_sd_prior);           // rho SD prior
+  DATA_SCALAR(rho_mean_prior);         // rho mean prior
+  DATA_SCALAR(tau_O_sd_prior);         // tau_O_ SD prior
+  DATA_SCALAR(tau_O_mean_prior);       // tau_O_ mean prior
 
   // Parameters
   PARAMETER(ln_global_linf);
@@ -49,7 +51,9 @@ Type objective_function<Type>::operator()()
   PARAMETER(global_tzero);
   PARAMETER(ln_sd_tzero);
 
-  PARAMETER(ln_global_omega);
+  PARAMETER(ln_b_sex);
+  PARAMETER_VECTOR(b_j_omega);
+
   PARAMETER(ln_sd_omega_lake);
   PARAMETER(ln_sd_omega_time);
 
@@ -81,8 +85,13 @@ Type objective_function<Type>::operator()()
     jnll -= dnorm(eps_omega_lake(l), Type(0.0), exp(ln_sd_omega_lake), true);
   }
   for (int t = 0; t < eps_omega_time.size(); t++) {
-    jnll -= dnorm(eps_omega_time(t), Type(0.0), exp(ln_sd_omega_time), true);
+     jnll -= dnorm(eps_omega_time(t), Type(0.0), exp(ln_sd_omega_time), true);
   }
+
+  // Derived quantities
+  Type Range = sqrt(8) / exp(ln_kappa);
+  Type SigO = 1 / sqrt(4 * M_PI * exp(2*ln_tau_O) * exp(2*ln_kappa));
+
   SparseMatrix<Type> Q = R_inla::Q_spde(spdeMatrices, exp(ln_kappa));
   jnll += SCALE(SEPARABLE(AR1(rho), GMRF(Q)), 1.0 / exp(ln_tau_O))(eps_omega_st);
 
@@ -90,29 +99,30 @@ Type objective_function<Type>::operator()()
   jnll -= dnorm(rho, rho_mean_prior, rho_sd_prior, true);
   jnll -= dnorm(ln_tau_O, tau_O_mean_prior, tau_O_sd_prior, true);
 
-  // Derived quantities
-  Type Range = sqrt(8) / exp(ln_kappa);
-  Type SigO = 1 / sqrt(4 * M_PI * exp(2*ln_tau_O) * exp(2*ln_kappa));
+  // calculate fixed effects for omega:
+  vector<Type> eta_fixed_i = X_ij_omega * b_j_omega;
 
   for (int i = 0; i < Nobs; i++) {
-    Type omega = exp(ln_global_omega +     // intercept
+    Type omega = exp(eta_fixed_i(i) +      // fixed effects
       eps_omega_lake(lake_i(i)) +          // std lake ran eff
       eps_omega_time(time_i(i)) +          // std time ran eff
       eps_omega_st(lake_i(i), time_i(i))); // ar1 space time
 
     Type linf = exp(ln_global_linf +       // intercept
+      ln_b_sex*sex_i(i) +                  // sex effect
       eps_linf(lake_i(i)));                // std ran eff
 
     Type t0 = global_tzero +      // intercept
-      eps_t0(lake_i(i));         // std ran eff
+      eps_t0(lake_i(i));          // std ran eff
 
     length_pred(i) = linf * (1 - exp(-(omega / linf) * (age_i(i) - t0)));
 
     if (!isNA(length_i(i)))
       jnll -= dlnorm(length_i(i), log(length_pred(i)), exp(ln_cv), true);
   }
+  REPORT(Range);
+  REPORT(SigO);
+  REPORT(rho);
   return jnll;
-  ADREPORT(Range)
-  ADREPORT(SigO)
 }
 
